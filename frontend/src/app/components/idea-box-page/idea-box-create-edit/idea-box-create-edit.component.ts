e78fc2fd2f6d67dfce4ae3a5ac9d74292fd6e751
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2023-05-09 14:42:09 +0200

    updated create-edit component for ideaBox

--- frontend/src/app/components/idea-box-page/idea-box-create-edit/idea-box-create-edit.component.ts
+++ frontend/src/app/components/idea-box-page/idea-box-create-edit/idea-box-create-edit.component.ts
@@ -1,13 +1,18 @@
+import { T } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { IdeaBoxDto } from 'src/app/models/dto/ideaBoxDto';
+import { IdeaSlimDto } from 'src/app/models/slimDto/ideaSlimDto';
 import { UserSlimDto } from 'src/app/models/slimDto/userSlimDto';
 import { WebResponse } from 'src/app/models/webResponse';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { IdeaService } from 'src/app/services/idea.service';
 import { IdeaBoxService } from 'src/app/services/ideaBox.service';
 import { SnackBarService } from 'src/app/services/snackBar.service';
+import { DeleteWarningComponent } from '../../popup/delete-warning/delete-warning.component';
 
 @UntilDestroy()
 @Component({
@@ -22,13 +27,18 @@ export class IdeaBoxCreateEditComponent implements OnInit {
     private auth: AuthService,
     private router: Router,
     private ideaBoxService: IdeaBoxService,
-    private route: ActivatedRoute
+    private ideaService: IdeaService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog
   ) {}
 
   id: string = '';
   user: UserSlimDto = null;
   ideaBox: IdeaBoxDto = null;
   isEdit: boolean = false;
+
+  displayedColumns: string[] = ['id', 'title', 'status', 'actions'];
+  dataSource: IdeaSlimDto[] = [];
 
   IdeaBoxForm = this.fb.group({
     name: ['', Validators.required],
@@ -81,6 +91,29 @@ export class IdeaBoxCreateEditComponent implements OnInit {
       });
   }
 
+  editIdea(id: number) {
+    this.router.navigateByUrl('/idea/' + id + '/edit');
+  }
+
+  deleteIdea(id: number) {
+    let dialogRef = this.dialog.open(DeleteWarningComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'Delete') {
+        this.ideaService
+          .deleteIdea$(id)
+          .pipe(untilDestroyed(this))
+          .subscribe((res: WebResponse<string>) => {
+            if (res.code == 200) {
+              this.snackBar.ok(res.message);
+              this.getIdeaBox();
+            } else {
+              this.snackBar.error(res.message);
+            }
+          });
+      }
+    });
+  }
+
   private getIdeaBox() {
     this.ideaBoxService
       .getIdeaBox$(this.id)
@@ -89,6 +122,7 @@ export class IdeaBoxCreateEditComponent implements OnInit {
         if (res.code == 200) {
           console.log(res.data);
           this.ideaBox = res.data;
+          this.dataSource = res.data.ideas;
           this.fillForm(res.data);
         } else {
           this.snackBar.error(res.message);
