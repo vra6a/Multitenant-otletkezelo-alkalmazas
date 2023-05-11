4aa5505273fb0c732bdd379eb9609b85705a9669
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2023-05-11 15:58:54 +0200

    user page started

--- frontend/src/app/components/user-page/user-page.component.ts
+++ frontend/src/app/components/user-page/user-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/dto/userDto';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { SnackBarService } from 'src/app/services/snackBar.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss'],
 })
 export class UserPageComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: SnackBarService
+  ) {}
 
-  constructor() { }
+  currentUser: User = null;
 
   ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
   }
-
 }
