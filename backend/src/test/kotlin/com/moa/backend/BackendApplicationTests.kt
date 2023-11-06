7fb084e7f2841334ede645d6513379599088b7b2
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2023-11-06 10:37:33 +0100

    test

--- backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
@@ -3,7 +3,7 @@ package com.moa.backend
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(properties = ["spring.config.name=application-test"])
+@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
