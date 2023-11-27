d7051adf4531a923de4fee4bcd77571b63cd5eff
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2023-11-27 22:20:09 +0100

    Update BackendApplicationTests.kt

--- backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
@@ -1,10 +1,20 @@
 package com.moa.backend
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@Value("\${spring.datasource.url}")
+	private val jdbcUrl: String? = null
+
+	@Value("\${spring.datasource.username}")
+	private val dbUsername: String? = null
+
+	@Value("\${spring.datasource.password}")
+	private val dbPassword: String? = null
 
 	@Test
 	fun alwaysTrue() {
