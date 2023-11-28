d0af0143233c9b22feeece809d9110be84ebd29c
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2023-11-28 22:25:46 +0100

    docker works

--- backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/moa/backend/BackendApplicationTests.kt
@@ -7,20 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Value("\${spring.datasource.url}")
-	private val jdbcUrl: String? = null
-
-	@Value("\${spring.datasource.username}")
-	private val dbUsername: String? = null
-
-	@Value("\${spring.datasource.password}")
-	private val dbPassword: String? = null
-
 	@Test
 	fun alwaysTrue() {
-		System.out.println("JDBC URL: " + jdbcUrl);
-		System.out.println("Username: " + dbUsername);
-		System.out.println("Password: " + dbPassword);
+
 	}
 
 }
