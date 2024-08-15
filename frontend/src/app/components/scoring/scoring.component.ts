d661758caabb88c02d371dc1d896e49ee697b4d4
Ádám Varga <varga.99.adam@gmail.com>
2024-08-15 20:49:42 +0200

    permission set for View Scored Idea Boxes

--- frontend/src/app/components/scoring/scoring.component.ts
+++ frontend/src/app/components/scoring/scoring.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSlimDto } from 'src/app/models/slimDto/userSlimDto';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-scoring',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoringComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) { }
 
+  currentUser: UserSlimDto = null
   ngOnInit(): void {
+    this.currentUser =  this.authService.getCurrentUser();
   }
 
 }
