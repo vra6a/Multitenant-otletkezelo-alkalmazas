5b86d127f680ff2ae9130148e63c2b59b9e34c61
Ádám Varga <varga.99.adam@gmail.com>
2024-10-23 21:30:33 +0200

    schema based multitenancy fully working - fe

--- frontend/src/app/interceptors/headerHandler.ts
+++ frontend/src/app/interceptors/headerHandler.ts
@@ -8,19 +8,22 @@ import {
 import { Injectable } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { Observable } from 'rxjs';
+import { TenantService } from '../services/multitenancy/tenantService';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private tenantService: TenantService) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    let tenant = this.tenantService.getTenantId()
     let token = window.sessionStorage.getItem('token');
     let headers = new HttpHeaders({
       Authorization: 'Bearer ' + token,
       'Content-Type': 'application/json',
+      'X-Tenant-Id': tenant
     });
     if (
       request.headers.get('Content-Type')?.toString() !==
